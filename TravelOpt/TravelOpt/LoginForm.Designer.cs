c2c5fe88b36a53584ffef0d9e9ff541626cbc26a
Caleb Yearsley <cnyearsley@outlook.com>
2016-10-07 13:34:25 -0600

    Made login page, started DB shtuff

---
+++ TravelOpt/TravelOpt/LoginForm.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TravelOpt
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginInGroupBox = new System.Windows.Forms.GroupBox();
+            this.signInButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.signUpGroupBox = new System.Windows.Forms.GroupBox();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.signUpLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.newUserNameLabel = new System.Windows.Forms.Label();
+            this.newEmailLabel = new System.Windows.Forms.Label();
+            this.newPasswordLabel = new System.Windows.Forms.Label();
+            this.retypePasswordLabel = new System.Windows.Forms.Label();
+            this.loginInGroupBox.SuspendLayout();
+            this.signUpGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginInGroupBox
+            // 
+            this.loginInGroupBox.Controls.Add(this.passwordLabel);
+            this.loginInGroupBox.Controls.Add(this.userNameLabel);
+            this.loginInGroupBox.Controls.Add(this.signInButton);
+            this.loginInGroupBox.Controls.Add(this.textBox2);
+            this.loginInGroupBox.Controls.Add(this.textBox1);
+            this.loginInGroupBox.Controls.Add(this.loginLabel);
+            this.loginInGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.loginInGroupBox.Location = new System.Drawing.Point(12, 6);
+            this.loginInGroupBox.Name = "loginInGroupBox";
+            this.loginInGroupBox.Size = new System.Drawing.Size(368, 373);
+            this.loginInGroupBox.TabIndex = 10;
+            this.loginInGroupBox.TabStop = false;
+            this.loginInGroupBox.Text = "Login";
+            // 
+            // signInButton
+            // 
+            this.signInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.signInButton.Location = new System.Drawing.Point(124, 255);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(121, 49);
+            this.signInButton.TabIndex = 12;
+            this.signInButton.Text = "Login";
+            this.signInButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(99, 190);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 30);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(99, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 30);
+            this.textBox1.TabIndex = 10;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.loginLabel.Location = new System.Drawing.Point(144, 63);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(79, 29);
+            this.loginLabel.TabIndex = 9;
+            this.loginLabel.Text = "Log In";
+            // 
+            // signUpGroupBox
+            // 
+            this.signUpGroupBox.Controls.Add(this.retypePasswordLabel);
+            this.signUpGroupBox.Controls.Add(this.newPasswordLabel);
+            this.signUpGroupBox.Controls.Add(this.newEmailLabel);
+            this.signUpGroupBox.Controls.Add(this.newUserNameLabel);
+            this.signUpGroupBox.Controls.Add(this.signUpButton);
+            this.signUpGroupBox.Controls.Add(this.textBox6);
+            this.signUpGroupBox.Controls.Add(this.textBox5);
+            this.signUpGroupBox.Controls.Add(this.textBox4);
+            this.signUpGroupBox.Controls.Add(this.textBox3);
+            this.signUpGroupBox.Controls.Add(this.signUpLabel);
+            this.signUpGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.signUpGroupBox.Location = new System.Drawing.Point(428, 12);
+            this.signUpGroupBox.Name = "signUpGroupBox";
+            this.signUpGroupBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.signUpGroupBox.Size = new System.Drawing.Size(368, 367);
+            this.signUpGroupBox.TabIndex = 11;
+            this.signUpGroupBox.TabStop = false;
+            this.signUpGroupBox.Text = "Sign Up";
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.signUpButton.Location = new System.Drawing.Point(130, 313);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(121, 49);
+            this.signUpButton.TabIndex = 15;
+            this.signUpButton.Text = "Sign Up";
+            this.signUpButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox6.Location = new System.Drawing.Point(99, 249);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(171, 30);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox5.Location = new System.Drawing.Point(99, 194);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(171, 30);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox4.Location = new System.Drawing.Point(99, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(171, 30);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox3.Location = new System.Drawing.Point(99, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(171, 30);
+            this.textBox3.TabIndex = 11;
+            // 
+            // signUpLabel
+            // 
+            this.signUpLabel.AutoSize = true;
+            this.signUpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.signUpLabel.Location = new System.Drawing.Point(138, 16);
+            this.signUpLabel.Name = "signUpLabel";
+            this.signUpLabel.Size = new System.Drawing.Size(99, 29);
+            this.signUpLabel.TabIndex = 10;
+            this.signUpLabel.Text = "Sign Up";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.userNameLabel.Location = new System.Drawing.Point(75, 101);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(83, 20);
+            this.userNameLabel.TabIndex = 13;
+            this.userNameLabel.Text = "Username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.passwordLabel.Location = new System.Drawing.Point(75, 167);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 20);
+            this.passwordLabel.TabIndex = 14;
+            this.passwordLabel.Text = "Password";
+            // 
+            // newUserNameLabel
+            // 
+            this.newUserNameLabel.AutoSize = true;
+            this.newUserNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.newUserNameLabel.Location = new System.Drawing.Point(74, 62);
+            this.newUserNameLabel.Name = "newUserNameLabel";
+            this.newUserNameLabel.Size = new System.Drawing.Size(118, 20);
+            this.newUserNameLabel.TabIndex = 15;
+            this.newUserNameLabel.Text = "New Username";
+            // 
+            // newEmailLabel
+            // 
+            this.newEmailLabel.AutoSize = true;
+            this.newEmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.newEmailLabel.Location = new System.Drawing.Point(74, 116);
+            this.newEmailLabel.Name = "newEmailLabel";
+            this.newEmailLabel.Size = new System.Drawing.Size(83, 20);
+            this.newEmailLabel.TabIndex = 16;
+            this.newEmailLabel.Text = "New Email";
+            // 
+            // newPasswordLabel
+            // 
+            this.newPasswordLabel.AutoSize = true;
+            this.newPasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.newPasswordLabel.Location = new System.Drawing.Point(74, 172);
+            this.newPasswordLabel.Name = "newPasswordLabel";
+            this.newPasswordLabel.Size = new System.Drawing.Size(78, 20);
+            this.newPasswordLabel.TabIndex = 17;
+            this.newPasswordLabel.Text = "Password";
+            // 
+            // retypePasswordLabel
+            // 
+            this.retypePasswordLabel.AutoSize = true;
+            this.retypePasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.retypePasswordLabel.Location = new System.Drawing.Point(74, 227);
+            this.retypePasswordLabel.Name = "retypePasswordLabel";
+            this.retypePasswordLabel.Size = new System.Drawing.Size(133, 20);
+            this.retypePasswordLabel.TabIndex = 18;
+            this.retypePasswordLabel.Text = "Retype Password";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(808, 391);
+            this.Controls.Add(this.signUpGroupBox);
+            this.Controls.Add(this.loginInGroupBox);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.loginInGroupBox.ResumeLayout(false);
+            this.loginInGroupBox.PerformLayout();
+            this.signUpGroupBox.ResumeLayout(false);
+            this.signUpGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox loginInGroupBox;
+        private System.Windows.Forms.Button signInButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.GroupBox signUpGroupBox;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label signUpLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label retypePasswordLabel;
+        private System.Windows.Forms.Label newPasswordLabel;
+        private System.Windows.Forms.Label newEmailLabel;
+        private System.Windows.Forms.Label newUserNameLabel;
+    }
+}
--- TravelOpt/TravelOpt/Program.cs
+++ TravelOpt/TravelOpt/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace TravelOpt
 {
@@ -14,9 +15,31 @@ namespace TravelOpt
         [STAThread]
         static void Main()
         {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=pellefant-02.db.elephantsql.com;Port=5432;UserId=lzukdgiw;Password=P3RvimsAf1G_GxCUVwNYGwaKPWSR5Dop;Database=lzukdgiw;");
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error :S");
+            }
+
+            // Do DB stuff here
+
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error :S");
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
